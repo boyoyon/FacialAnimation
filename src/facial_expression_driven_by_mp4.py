3e2700010ef337444dd547e2af2bcf502332a839
boyoyon <nishimaki.hidetoshi@gmail.com>
2025-01-23 16:35:23 +0900

    Add files via upload

---
+++ src/facial_expression_driven_by_mp4.py
@@ -0,0 +1,95 @@
+import torch
+from _animate import normalize_kp
+from _demo import load_checkpoints
+import numpy as np
+import cv2, glob, os, sys
+
+cpu = False if torch.cuda.is_available() else True
+
+checkpoint_path = os.path.join(os.path.dirname(__file__), 'vox-cpk.pth.tar')
+
+argv = sys.argv
+argc = len(argv)
+
+print('%s changing facial expression of the face within the image' % argv[0])
+print('[usage] python %s <face image> <drive video>' % argv[0])
+
+if argc < 3:
+    quit()
+
+source_image = cv2.imread(argv[1])
+source_image = cv2.resize(source_image, (256, 256))
+source_image = source_image.astype(np.float32) / 255.0
+
+generator, kp_detector = load_checkpoints(config_path=os.path.join(os.path.dirname(__file__), 'config/vox-256.yaml'), checkpoint_path=checkpoint_path, cpu=cpu)
+
+output_folder = 'result'
+
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+
+relative=True
+adapt_movement_scale=True
+
+cap = cv2.VideoCapture(argv[2])
+
+with torch.no_grad() :
+    predictions = []
+    source = torch.tensor(source_image[np.newaxis]).permute(0, 3, 1, 2)
+    
+    if not cpu:
+        source = source.cuda()
+    kp_source = kp_detector(source)
+    count = 1
+
+    ret = True
+    while ret:
+       
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (256, 256))
+        frame = frame.astype(np.float32) / 255.0
+            
+        if count == 1:
+            source_image1 = frame
+            source1 = torch.tensor(source_image1[np.newaxis]).permute(0, 3, 1, 2)
+            kp_driving_initial = kp_detector(source1)
+        
+        frame_test = torch.tensor(frame[np.newaxis]).permute(0, 3, 1, 2)
+
+        driving_frame = frame_test
+        
+        if not cpu:
+            driving_frame = driving_frame.cuda()
+        
+        kp_driving = kp_detector(driving_frame)
+        kp_norm = normalize_kp(kp_source=kp_source,
+                            kp_driving=kp_driving,
+                            kp_driving_initial=kp_driving_initial, 
+                            use_relative_movement=relative,
+                            use_relative_jacobian=relative, 
+                            adapt_movement_scale=adapt_movement_scale)
+        
+        out = generator(source, kp_source=kp_source, kp_driving=kp_norm)
+        
+        predictions.append(np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
+        
+        im = np.transpose(out['prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0]
+        im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
+        
+        img = cv2.cvtColor(im, cv2.COLOR_RGB2BGR) 
+        
+        cv2.imshow('Facial Expression',img)
+
+        dst_path = os.path.join(output_folder, '%04d.png' % count)
+        dst = img * 255
+        dst = np.clip(dst, 0, 255)
+        dst = dst.astype(np.uint8)
+        cv2.imwrite(dst_path, dst)
+        print('save %s' % dst_path)
+
+        count += 1
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cv2.destroyAllWindows()
